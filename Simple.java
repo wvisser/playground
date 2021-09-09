b9877aaeaa5e2476742a152332b0782bd2426494
Willem Visser <vissie@amazon.com>
2021-09-09 13:04:03 -0700

    Just a push test on public this time

--- Simple.java
+++ Simple.java
@@ -1,6 +1,7 @@
 class Simple {
 
   public void foo(Person p) {
+    // nothing more
     log.info("Person name and email: " + p.getFirstName() + ", " + p.getLastName() + ", " + p.getEmailAddress()); // no change
   }
 }
