f858cb4aafe655463324e4c087f62e87a7fea72e
Willem Visser <vissie@amazon.com>
2021-09-09 14:05:10 -0700

    Must trigger

---
+++ EqTest.java
@@ -0,0 +1,11 @@
+public class EqTest {
+    public boolean eqOp() {
+        String s = "abc";
+        if (s != null) { //should not flag
+            if (s == "abc") { //should flag
+                return true;
+            }
+        }
+        return false;
+    }
+}
