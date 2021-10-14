eab9ad29245ab0145e8f2bbbc84651ffa8a11d3d
Willem Visser <willem@gmail.com>
2021-10-14 10:42:17 -0700

    Delete Simple.java

--- Simple.java
+++
@@ -1,8 +0,0 @@
-class Simple {
-
-  public void foo(Person p) {
-    // nothing more
-    // nothing more
-    log.info("Person name and email: " + p.getFirstName() + ", " + p.getLastName() + ", " + p.getEmailAddress()); // no change
-  }
-}
