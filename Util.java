b2879085ce993d84552c0fadc956efc873107a3e
Willem Visser <willem@gmail.com>
2021-10-14 10:49:31 -0700

    Update Util.java

--- Util.java
+++ Util.java
@@ -4,20 +4,22 @@ public class Util {
 
     public static Object get() {
         if ("1" == "1") //expect code-guru comment here
+            System.out.println("Yes");
         return null;
     }
     
     //repeat the same method to get a code-guru comment
     public static Object get2() {
         if ("1" == "1") //expect code-guru comment here
+            System.out.println("Yes");
         return null;
     }
 
     //try to get a code-guru comment
-    private static String getString() {
+    private static String getString(String input) {
         java.util.ArrayList unusedList1 = new java.util.ArrayList();
         java.util.ArrayList unusedList2 = new java.util.ArrayList();
-        String abcdefg;
+        String abcdefg = input;
         //expect code-guru comment here
         System.out.println(abcdefg.length());
         return "1"+"2";
